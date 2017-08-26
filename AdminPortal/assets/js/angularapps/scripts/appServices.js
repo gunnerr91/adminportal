34d6be6a80510b187ed26959b1ac767410431600
Muktadiru Ahmed <blissvortex@hotmail.com>
2017-08-26 20:54:39 +0100

    updated appservice.js

--- AdminPortal/assets/js/angularapps/scripts/appServices.js
+++ AdminPortal/assets/js/angularapps/scripts/appServices.js
@@ -2,6 +2,26 @@
     return {
         load: function (year) {
             return $http.get("/webapi/yearly-wage-exp/get-table-data/" + year);
+        },
+
+        addNewEmployee: function (jsonfiedData) {
+            return $http.post("/webapi/yearly-wage-exp/quick-add-new-employee", jsonfiedData);
+        },
+
+        quickAddNewEntry: function (jsonfiedData, year) {
+            return $http.post("/webapi/yearly-wage-exp/quick-add-new-entry/" + year, jsonfiedData);
+        },
+
+        getEmployeeList: function () {
+            return $http.get("/webapi/yearly-wage-exp/get-employee-list-for-new-entry");
+        },
+
+        saveEntryAmendments: function (jsonfiedData) {
+            return $http.put("/webapi/yearly-wage-exp/put-entry-details-for-edit", jsonfiedData);
+        },
+
+        deleteEntry: function (yearId) {
+            return $http.delete("/webapi/yearly-wage-exp/delete/" + yearId);
         }
     }
 }]);
