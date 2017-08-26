4583d80e3acefd4d61cccbdb11b5100cc44fe45f
Muktadiru Ahmed <blissvortex@hotmail.com>
2017-08-26 19:20:43 +0100

    Set up the bare basic of the Angular  App, updated service for the load functionality, tested with the default data.

--- AdminPortal/assets/js/angularapps/scripts/appController.js
+++ AdminPortal/assets/js/angularapps/scripts/appController.js
@@ -1,3 +1,17 @@
-﻿ngSpaApp.controller("ngSpaAppController", function ($scope) {
+﻿ngSpaApp.controller("ngSpaAppController", function ($scope, ngSpaAppService) {
     $scope.test = "hello world";
+    $scope.init = function () {
+        $scope.load();
+    }
+
+    $scope.load = function () {
+        ngSpaAppService
+            .load()
+            .then(function (response) {
+                $scope.YearList = response.data;
+                console.log("service loaded, check below");
+                console.log(response.data);
+            })
+    }
+
 });
--- AdminPortal/assets/js/angularapps/scripts/appServices.js
+++ AdminPortal/assets/js/angularapps/scripts/appServices.js
@@ -1,1 +1,7 @@
-﻿
+﻿ngSpaApp.service('ngSpaAppService', ["$http", function ($http){
+    return {
+        load: function (year) {
+            return $http.get("/webapi/yearly-wage-exp/get-table-data/" + year);
+        }
+    }
+}]);
